4ef1fdc024848f4cd5e5610f3e2038e60bd9d8ef
mah1shah <mahidshah08@gmail.com>
2026-09-15 17:01:07 +0530

    Update eslint.config.js

--- frontend/eslint.config.js
+++ frontend/eslint.config.js
@@ -9,7 +9,7 @@ export default defineConfig([
   {
     files: ['**/*.{js,jsx}'],
     extends: [
-      js.configs.recommended,
+      js.configs.recomended,
       reactHooks.configs.flat.recommended,
       reactRefresh.configs.vite,
     ],
