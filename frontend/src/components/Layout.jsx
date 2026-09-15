e72b646665ff0b7654672442605d9ce47f256260
mah1shah <mahidshah08@gmail.com>
2026-09-15 17:07:46 +0530

    Update Layout.jsx

--- frontend/src/components/Layout.jsx
+++ frontend/src/components/Layout.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { Outlet } from 'react-router-dom';
 import Navbar from './Navbar';
 import Footer from './Footer';
-import './Layout.cs';
+import './Layout.css';
 
 const Layout = () => {
   return (
